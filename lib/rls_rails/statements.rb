30bc60a7a8a071982a922924d73abf6db96a4226
Stephan Biastoch <biastoch@corpshaus2.de>
2019-03-11 12:12:45 +0100

    Refactores migration statements and fixes rollback of policies

--- lib/rls_rails/statements.rb
+++ lib/rls_rails/statements.rb
@@ -4,59 +4,29 @@ module RLS
 
     def enable_rls table, force: false
       reversible do |dir|
-        dir.up do
-          q = "ALTER TABLE #{table} ENABLE ROW LEVEL SECURITY#{force ? ', FORCE ROW LEVEL SECURITY' : ''};"
-          execute q
-        end
-
-        dir.down do
-          q = "ALTER TABLE #{table} DISABLE ROW LEVEL SECURITY#{force ? ', NO FORCE ROW LEVEL SECURITY' : ''};"
-          execute q
-        end
+        dir.up   { do_enable_rls  table, force: force }
+        dir.down { do_disable_rls table, force: force }
       end
     end
 
     def disable_rls table, force: false
       reversible do |dir|
-        dir.up do
-          q = "ALTER TABLE #{table} DISABLE ROW LEVEL SECURITY#{force ? ', NO FORCE ROW LEVEL SECURITY' : ''};"
-          execute q
-        end
-
-        dir.down do
-          q = "ALTER TABLE #{table} ENABLE ROW LEVEL SECURITY#{force ? ', FORCE ROW LEVEL SECURITY' : ''};"
-          execute q
-        end
+        dir.up   { do_disable_rls table, force: force }
+        dir.down { do_enable_rls  table, force: force }
       end
     end
 
     def create_policy table, version: 1
-      RLS.clear_policies!
       reversible do |dir|
-        dir.up do
-          enable_rls table, force: true
-          load policy_path(table, version)
-          execute RLS.create_sql(table)
-        end
-
-        dir.down do
-          drop_policy table, version: version
-        end
+        dir.up   { do_create_policy table, version: version }
+        dir.down { do_drop_policy   table, version: version }
       end
     end
 
     def drop_policy table, version: nil
-      RLS.clear_policies!
-
       reversible do |dir|
-        dir.up do
-          load policy_path(table, version || last_version_of(table))
-          execute RLS.drop_sql(table)
-        end
-
-        dir.down do
-          create_policy table, version: version
-        end
+        dir.up   { do_drop_policy   table, version: version }
+        dir.down { do_create_policy table, version: version }
       end
     end
 
@@ -65,32 +35,53 @@ module RLS
 
       reversible do |dir|
         dir.up do
-          # Drop existing policies
-          existing_policies = execute("SELECT policyname FROM pg_policies WHERE tablename = '#{table}'").values.flatten
-          existing_policies.each do |policy_name|
-            execute("DROP POLICY  #{policy_name} ON #{table};")
-          end
-
-          # Create current policies
-          file = version ? policy_path(table, version) : policy_path(table, last_version_of(table) + 1)
-          load(file)
-          sql = RLS.create_sql(table)
-          puts sql
-          execute sql
+          drop_policies_for table
+          do_create_policy(table, version: version || last_version_of(table) + 1)
         end
 
         dir.down do
-          version = revert_to_version || last_version_of(table) - 1
-          raise ActiveRecord::IrreversibleMigration, 'update_policy: revert_to_version missing!' if revert_to_version.nil? && version > 0
-          file = policy_path(table, version)
-          load(file)
-          if version > 0
-            execute RLS.create_sql(table)
+          new_version = revert_to_version || last_version_of(table) - 1
+          raise ActiveRecord::IrreversibleMigration, 'update_policy: revert_to_version missing!' if revert_to_version.nil? && new_version > 0
+          if new_version > 0
+            do_drop_policy table, version: version
+            do_create_policy table, version: new_version
           else
-            execute RLS.drop_sql(table)
+            do_drop_policy table, version: version
           end
         end
       end
+    end
+
+    private
+
+    def drop_policies_for table
+      existing_policies = execute("SELECT policyname FROM pg_policies WHERE tablename = '#{table}'").values.flatten
+      existing_policies.each do |policy_name|
+        execute "DROP POLICY #{policy_name} ON #{table};"
+      end
+    end
+
+    def do_create_policy table, version: nil
+      RLS.clear_policies!
+      enable_rls table, force: true
+      load policy_path(table, version)
+      execute RLS.create_sql(table)
+    end
+
+    def do_drop_policy table, version: nil
+      RLS.clear_policies!
+      load policy_path(table, version || last_version_of(table))
+      execute RLS.drop_sql(table)
+    end
+
+    def do_enable_rls table, force: false
+      q = "ALTER TABLE #{table} ENABLE ROW LEVEL SECURITY#{force ? ', FORCE ROW LEVEL SECURITY' : ''};"
+      execute q
+    end
+
+    def do_disable_rls table, force: false
+      q = "ALTER TABLE #{table} DISABLE ROW LEVEL SECURITY#{force ? ', NO FORCE ROW LEVEL SECURITY' : ''};"
+      execute q
     end
   end
 end
