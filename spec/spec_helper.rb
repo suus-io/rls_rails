25dea8c0cc55b6db3a8124ee6e2341a6dd22b317
Stephan Biastoch <biastoch@corpshaus2.de>
2022-04-08 17:18:50 +0200

    Setup Github Actions (#16)
    Setup Github Actions workflow to run tests for each commit and PR to master.
    Workflow file is based on https://github.com/scenic-views/scenic/blob/main/.github/workflows/ci.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,16 +10,6 @@ Bundler.setup
 
 require 'rls_rails' # and any other gems you need
 
-def load_schema
-  config = YAML::load(IO.read(File.dirname(__FILE__) + '/dummy/config/database.yml'))
-  ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
-
-  ActiveRecord::Base.establish_connection #(config[db_adapter])
-  load(File.dirname(__FILE__) + "/dummy/db/schema.rb")
-end
-
-load_schema
-
 RSpec.configure do |config|
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
